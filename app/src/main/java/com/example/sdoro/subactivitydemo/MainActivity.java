77d7225ba30604ed3a2b6e29567117394eb34858
sdoro <sandro.doro@gmail.com>
2017-04-17 21:18:53 +0200

    Init

---
+++ app/src/main/java/com/example/sdoro/subactivitydemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sdoro.subactivitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button button = new Button(this);
+        button.setText("Lancia SubActivity");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSubActivity();
+            }
+        });
+        setContentView(button);
+    }
+    private void startSubActivity() {
+        Intent intent = new Intent(this, SubActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/sdoro/subactivitydemo/SubActivity.java
@@ -0,0 +1,26 @@
+package com.example.sdoro.subactivitydemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button button = new Button(this);
+        button.setText("Termina SubActivity");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        setContentView(button);
+    }
+}
